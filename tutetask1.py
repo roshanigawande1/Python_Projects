c085820a2e08d9d7b2f206549654b47b8fec8542
Roshani Gawande <roshanigawande2128@gmail.com>
2025-07-29 20:41:48 +0530

    1st project did

---
+++ tutetask1.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number : "))
+b=int(input("Enter the Second Nmuber : "))
+
+print("Addition : ",a+b)
+print("Substraction : ",a-b)
+print("Multiplication : ",a*b)
+print("divion : ",a/b)
